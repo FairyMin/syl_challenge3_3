a738b82d670c259898436c45774b93036adccafc
mzg <15122302865@163.com>
2018-03-29 21:04:32 +0800

    完成端口扫描

--- scan.py
+++ scan.py
@@ -4,13 +4,15 @@ import socket
 import getopt
 import sys
 
+
 def scan(ip,port):
     test_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    result = test_sock.connect_ex((ip,port))
-    if result == 0:
-        print("Port is open")
-    else:
-        print("Port is closed")
+    for i in port:
+        result = test_sock.connect_ex((ip,int(i)))
+        if result == 0:
+            print("%s open"%i)
+        else:
+            print("%s closed"%i)
 
 
 if __name__=='__main__':
@@ -19,6 +21,31 @@ if __name__=='__main__':
     except getopt.GetoptError:
         print("Parameter Error")
         sys.exit(2)
-    print(opts)
-    print(args)
-    #scan(ip,port)
+    
+    for o,a in opts:
+        #判断host地址是够符合规则
+        if o == "--host":
+            ip_l = a.split('.',3)
+            if len(ip_l) == 4:
+                for i in ip_l:
+                    if i.isdigit() is False:
+                        print("Parameter Error")
+                        sys.exit(2)
+                host=a
+            else:
+                print("Parameter Error")
+                sys.exit(2)
+        #获取port的列表 
+        elif o == "--port":
+            try:
+                if "-" in a:
+                    range_port = a.split("-",1)
+                    port = [x for x in range(int(range_port[0]),int(range_port[1]))]
+                else:
+                    port = [a]
+            except:
+                print("Parameter Error")
+                sys.exit(2)
+        else:
+            assert False,"Unhandled option"
+    scan(host,port)
